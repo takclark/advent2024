ed5661ffb1abe044a75670191070cef14ae8cf4b
Tyler Clark <tyler@justworkslabs.com>
2024-12-06 01:19:02 -0500

    both

--- 6/main.go
+++ 6/main.go
@@ -18,18 +18,24 @@ func solve(input string) (int, int) {
 	var total1, total2 int
 
 	m := parseInput(input)
-	fmt.Println(m)
+	initialGuardDir := m.guardDir
+	initialGuardPos := m.guardPos
+	m.traverse()
+	total1 = len(m.visited)
 
-	for m.tick() {
-		fmt.Println(m.grid)
+	// It's only possible to affect the path by placing an obstacle in a position that was visited. We're going to brute force but at lease narrow it down to those.
+	candidates := []tuple{}
+	for k := range m.visited {
+		if k.x == initialGuardPos.x && k.y == initialGuardPos.y {
+			continue
+		}
+		candidates = append(candidates, k)
 	}
 
-	for i := range len(m.grid) {
-		for j := range len(m.grid[i]) {
-			if m.grid[i][j] == "X" {
-				total1++
-			}
-		}
+	for _, c := range candidates {
+		m.wipe(initialGuardDir, initialGuardPos)
+		m.grid[c.y][c.x] = "O"
+		total2 += m.traverse()
 	}
 
 	return total1, total2
@@ -55,32 +61,57 @@ func parseInput(input string) *labMap {
 		grid:     grid,
 		guardPos: startingGuardPos,
 		guardDir: tuple{x: 0, y: -1},
+		visited:  map[tuple]map[tuple]struct{}{},
 	}
 }
 
-func (m *labMap) tick() bool {
-	m.grid[m.guardPos.y][m.guardPos.x] = "X"
+func (m *labMap) traverse() int {
+	for {
+		if dirs, ok := m.visited[m.guardPos]; ok {
+			if _, dok := dirs[m.guardDir]; dok {
+				// cycle, exit
+				return 1
+			}
+		} else {
+			m.visited[m.guardPos] = map[tuple]struct{}{}
+		}
+		m.visited[m.guardPos][m.guardDir] = struct{}{}
 
-	next := tuple{
-		x: m.guardPos.x + m.guardDir.x,
-		y: m.guardPos.y + m.guardDir.y,
+		m.grid[m.guardPos.y][m.guardPos.x] = "X"
+
+		next := tuple{
+			x: m.guardPos.x + m.guardDir.x,
+			y: m.guardPos.y + m.guardDir.y,
+		}
+
+		if next.x < 0 || next.x >= len(m.grid[0]) {
+			return 0
+		}
+
+		if next.y < 0 || next.y >= len(m.grid) {
+			return 0
+		}
+
+		if m.grid[next.y][next.x] == "#" || m.grid[next.y][next.x] == "O" {
+			m.guardDir = turn(m.guardDir)
+		} else {
+			m.guardPos = next
+		}
+
 	}
+}
 
-	if next.x < 0 || next.x >= len(m.grid[0]) {
-		return false
+func (m *labMap) wipe(dir, pos tuple) {
+	m.visited = map[tuple]map[tuple]struct{}{}
+	m.guardDir = dir
+	m.guardPos = pos
+	for i := range m.grid {
+		for j := range m.grid[i] {
+			if m.grid[i][j] == "X" || m.grid[i][j] == "O" {
+				m.grid[i][j] = "."
+			}
+		}
 	}
-
-	if next.y < 0 || next.y >= len(m.grid) {
-		return false
-	}
-
-	if m.grid[next.y][next.x] == "#" {
-		m.guardDir = turn(m.guardDir)
-	} else {
-		m.guardPos = next
-	}
-
-	return true
 }
 
 func turn(t tuple) tuple {
@@ -103,4 +134,5 @@ type labMap struct {
 	grid     [][]string
 	guardPos tuple
 	guardDir tuple // dx, dy
+	visited  map[tuple]map[tuple]struct{}
 }
